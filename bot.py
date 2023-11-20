1d8cd22fc6c736bd1fefbd448dbe49e3293e7bce
BREWDxyz <dad@brewd.xyz>
2023-11-20 12:24:00 -0500

    added debug

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import datetime
+import json
 
 # Environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,8 +18,8 @@ IMGUR_CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
 
 # Initialize MongoDB client
 mongo_client = MongoClient(MONGODB_CONN_STRING)
-db = mongo_client.image_generation_db  # Replace with your actual MongoDB database name
-collection = db.generated_images  # Replace with your actual collection name
+db = mongo_client.image_generation_db  # Replace with your MongoDB database name
+collection = db.generated_images  # Replace with your MongoDB collection name
 
 # Initialize Hugging Face Inference Client for SDXL
 sdxl_client = InferenceClient(model="stabilityai/stable-diffusion-xl-base-1.0", token=HUGGINGFACE_TOKEN)
@@ -41,7 +42,8 @@ async def on_ready():
 async def generate_image(ctx, *, prompt: str):
     try:
         # Generate image using Hugging Face model
-        image = sdxl_client.text_to_image(prompt, guidance_scale=9)
+        response = sdxl_client(text=prompt, guidance_scale=9)
+        image = response.get('image')  # Assumes 'image' key in the response
         image_url = save_image_to_imgur(image)
 
         # Log and save to MongoDB
@@ -53,27 +55,24 @@ async def generate_image(ctx, *, prompt: str):
         }
         collection.insert_one(document)
 
-        await ctx.send(image_url)
+        await ctx.send(image_url or "Image generation failed.")
     except Exception as e:
-        logging.error(f'Error generating image: {e}')
+        logging.error(f'Error in generate_image: {e}')
         await ctx.send("Sorry, I couldn't process the image generation.")
 
-def save_image_to_imgur(image):
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = buffered.getvalue()
-
+def save_image_to_imgur(image_data):
     headers = {'Authorization': f'Client-ID {IMGUR_CLIENT_ID}'}
-    response = requests.post(
-        'https://api.imgur.com/3/image',
-        headers=headers,
-        data={'image': img_str}
-    )
+    response = requests.post('https://api.imgur.com/3/image', headers=headers, files={'image': image_data})
+
+    logging.info(f'Imgur response: {response.status_code}, {response.text}')
 
     if response.status_code == 200:
-        return response.json()['data']['link']
+        try:
+            return response.json()['data']['link']
+        except json.JSONDecodeError:
+            logging.error(f'JSON parsing error: {response.text}')
     else:
-        logging.error(f'Imgur upload failed: {response.content}')
-        return None
+        logging.error(f'Imgur upload failed: {response.text}')
+    return None
 
 bot.run(DISCORD_TOKEN)
