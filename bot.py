1133891e4f05d3f67fad4e563d6e924fd5b7bf81
BREWDxyz <dad@brewd.xyz>
2023-11-20 11:43:11 -0500

    updated image gen function

--- bot.py
+++ bot.py
@@ -1,66 +1,79 @@
 import os
 import discord
 from discord.ext import commands
-import requests
+from huggingface_hub import InferenceClient
 from pymongo import MongoClient
 import logging
+import requests
+from PIL import Image
+from io import BytesIO
+import datetime
 
-# Setting up environment variables
-MONGODB_CONN_STRING = os.getenv('MONGODB_CONN_STRING')
-HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN')
+# Environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN')
+MONGODB_CONN_STRING = os.getenv('MONGODB_CONN_STRING')
+IMGUR_CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
 
-# Initialize Logging
+# Initialize MongoDB client
+mongo_client = MongoClient(MONGODB_CONN_STRING)
+db = mongo_client.image_generation_db  # Replace with your actual MongoDB database name
+collection = db.generated_images  # Replace with your actual collection name
+
+# Initialize Hugging Face Inference Client for SDXL
+sdxl_client = InferenceClient(model="stabilityai/stable-diffusion-xl-base-1.0", token=HUGGINGFACE_TOKEN)
+
+# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize MongoDB Connection
-try:
-    mongo_client = MongoClient(MONGODB_CONN_STRING)
-    db = mongo_client['discordBotDB']
-    collection = db['userPreferences']  # Example collection
-    logging.info("MongoDB connected successfully.")
-except Exception as e:
-    logging.error(f"Error connecting to MongoDB: {e}")
-    exit(1)
-
-# Define Intents for the Bot
+# Discord bot setup with intents
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
-intents.guilds = True
-
-# Discord Bot Setup
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     logging.info(f'{bot.user.name} has connected to Discord!')
+    logging.info('MongoDB connected successfully.')
 
 @bot.command(name='generate', help='Generates an image from a prompt')
 async def generate_image(ctx, *, prompt: str):
     try:
-        image_url = call_huggingface_api(prompt)
+        # Generate image using Hugging Face model
+        image = sdxl_client.text_to_image(prompt, guidance_scale=9)
+        image_url = save_image_to_imgur(image)
+
+        # Log and save to MongoDB
+        document = {
+            "user_id": ctx.author.id,
+            "prompt": prompt,
+            "image_url": image_url,
+            "timestamp": datetime.datetime.utcnow()
+        }
+        collection.insert_one(document)
+
         await ctx.send(image_url)
     except Exception as e:
+        logging.error(f'Error generating image: {e}')
         await ctx.send("Sorry, I couldn't process the image generation.")
-        logging.error(f"Error in generate_image: {e}")
 
-def call_huggingface_api(prompt):
-    try:
-        API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-        headers = {"Authorization": f"Bearer {HUGGINGFACE_TOKEN}"}
-        data = {"inputs": prompt}
+def save_image_to_imgur(image):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = buffered.getvalue()
 
-        response = requests.post(API_URL, headers=headers, json=data)
-        response.raise_for_status()  # Raises HTTPError for bad requests
-        image_url = response.json()[0]  # Extract image URL from response
-        return image_url
-    except requests.exceptions.RequestException as e:
-        logging.error(f"API request error: {e}")
-        raise
+    headers = {'Authorization': f'Client-ID {IMGUR_CLIENT_ID}'}
+    response = requests.post(
+        'https://api.imgur.com/3/image',
+        headers=headers,
+        data={'image': img_str}
+    )
 
-if __name__ == "__main__":
-    if DISCORD_TOKEN is None:
-        logging.error("DISCORD_TOKEN is not set. Please set the token and retry.")
-        exit(1)
-    bot.run(DISCORD_TOKEN)
+    if response.status_code == 200:
+        return response.json()['data']['link']
+    else:
+        logging.error(f'Imgur upload failed: {response.content}')
+        return None
+
+bot.run(DISCORD_TOKEN)
